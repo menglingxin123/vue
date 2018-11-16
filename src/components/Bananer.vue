b844175fc4ed76d9115cd212d79dd0256779dcb3
two <33169296+menglingxin123@users.noreply.github.com>
2018-11-16 23:01:49 +0800

    meng

--- src/components/Bananer.vue
+++ src/components/Bananer.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-
+    <button @click="add">{{ count }}</button>
+    <button @click="reduce">{{ count1 }}</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Bananer',
-  data () {
-    return {
-      msg:'bananer'
+    name: 'Bananer',
+    data () {
+        return {
+             msg:'bananer'
+        }
+    },
+    computed:{
+        count(){
+            return this.$store.state.count
+        },
+        count1(){
+            return this.$store.state.count1
+        }
+    },
+    methods:{
+        add(){
+            this.$store.commit('increment')
+        },
+        reduce(){
+            this.$store.commit('reduce')
+        }
     }
-  }
 }
 </script>
 
